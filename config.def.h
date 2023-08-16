b859ca05f05c52916335648312277aae69fcc5f0
Bartosz Chowaniak <chowaniak.bartosz@protonmail.com>
2023-08-16 18:50:36 +0200

    set user and group

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "user";
+static const char *group = "user";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
